7ee6964b556ea841f774bedfaec9cc4023188fba
Bangbang123123 <romulo.gamedesigner@gmail.com>
2025-12-01 22:48:40 -0300

    tempo to hours

--- TimeTracker/src/desempenho/Desempenho.java
+++ TimeTracker/src/desempenho/Desempenho.java
@@ -59,7 +59,7 @@ public class Desempenho {
                     }
                 }
 
-                double time_count = Duration.between(t.getDataInicial(), t.getDataFinal()).toMinutes();
+                double time_count = Duration.between(t.getDataInicial(), t.getDataFinal()).toMinutes() / 60;
 
                 if (exists != -1) {
                         tempo_gasto.set(exists, tempo_gasto.get(exists) + time_count);
@@ -98,7 +98,7 @@ public class Desempenho {
                     }
                 }
 
-                double time_count = Duration.between(t.getDataInicial(), t.getDataFinal()).toMinutes();
+                double time_count = Duration.between(t.getDataInicial(), t.getDataFinal()).toMinutes() / 60;
 
                 if (exists != -1) {
                         tempo_gasto.set(exists, tempo_gasto.get(exists) + time_count);
