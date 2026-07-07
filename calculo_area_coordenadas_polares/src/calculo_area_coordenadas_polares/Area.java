9968f3df31b5e856272cb95cec6039434c583167
Wiilll7 <wii227777@gmail.com>
2026-07-07 15:57:50 -0300

    a

--- calculo_area_coordenadas_polares/src/calculo_area_coordenadas_polares/Area.java
+++ calculo_area_coordenadas_polares/src/calculo_area_coordenadas_polares/Area.java
@@ -2,43 +2,23 @@ package calculo_area_coordenadas_polares;
 
 public class Area {
 	public static double function(double theta) {
-		return 4 - 4 * Math.cos(theta);
+		return Math.cos(2*theta);
 	}
 	
 	public static double riemann_sum(double n, double b, double a) {
 		double step = (b-a)/n;
 		double sum = 0;
 		
-		double progress = 0;
 		for (double i = a; i <= b; i += step) {
 			sum += Math.pow(function(i),2) * step;
-			if (progress == 10000) {
-				progress = 0;
-				progress_bar(i/b);
-			}
-			progress++;
 		}
 		return sum/2.0;
 	}
 	
-	public static void progress_bar(double percentage) {
-		String content = "\r[";
-		percentage *= 50;
-		for (int i = 0; i <= 50; i++) {
-			if (percentage >= i) {
-				content += '#';
-			} else {
-				content += '.';
-			}
-		}
-		content += "]";
-		System.out.print(content);
-	}
-	
 	public static void main(String[] args) {
 		System.out.println("\nRiemann Sum: "+
-				riemann_sum(200000000.0,Math.PI*2,0)
-				+"\nIntegral: "+ Math.PI*24
+				riemann_sum(1.5, Math.PI/4, -Math.PI/4)
+				+"\nIntegral:    "+ Math.PI/8
 		);
 	}
 }
